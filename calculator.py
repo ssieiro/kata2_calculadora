e81777c4a490fcbe0ef83c0b773251a72b891637
Sonia Sieiro Díaz <soniasieirod@gmail.com>
2019-06-25 21:01:24 +0200

    inicio martes

--- calculator.py
+++ calculator.py
@@ -13,13 +13,14 @@ class CalcDisplay (ttk.Frame): #Display donde sale el resultado
         s = ttk.Style()
         s.configure('my.TLabel', font="Helvetica 42") #definimos un estilo genérico al que llamamos con el nombre 'my.TLabel'
 
-        self.lblDisplay = ttk.Label(self, text='0', style='my.TLabel', anchor=E) #al hacer esto nos coge el estilo de s.configure
+        self.lblDisplay = ttk.Label(self, text='0', style='my.TLabel', anchor=E, foreground= "white")
+        #al hacer esto nos coge el estilo de s.configure
         self.lblDisplay.pack(fill=BOTH, expand=True)
 
 class CalcButton(ttk.Frame):
     def __init__(self, parent, **kwargs):
         ttk.Frame.__init__(self, parent, height=_heightBtn, width=_widthBtn)
-        self.pack_propagate(0)
+        self.pack_propagate(0) #en 0 los botones se expanden, en 1 no lo hacen y cogen su tamaño por defecto
 
         self.button = ttk.Button(self, text=kwargs['text'], command=kwargs['command'])
         #le estamos diciendo que va a haber que mandarle un texto(que irá dentro del botón)
@@ -32,7 +33,7 @@ class Calculator(ttk.Frame):
         self.display = CalcDisplay(self) #Instancia de objeto Display
         self.display.grid(column=0, row=0, columnspan=4)
 
-        CalcButton(self, text='C', command=None).grid(column=0, row=1)
+        CalcButton(self, text='C', command=None).grid(column=0, row=1) #instancias de cada botón
         CalcButton(self, text='+/-', command=None).grid(column=1, row=1)
         CalcButton(self, text='%', command=None).grid(column=2, row=1)
         CalcButton(self, text='÷', command=None).grid(column=3, row=1)
